b7ded3a6014b46f4035e6dfde20f1fc79cc0a173
Hassanshahzad-x <hassanshahzad3513@gmail.com>
2025-08-08 02:57:46 +0500

    backend configurations added

---
+++ backend/routes.py
@@ -0,0 +1,10 @@
+from flask_restful import Api
+from handlers.upload import FileUpload
+from handlers.ask import AskQuestion
+from handlers.remove import RemoveFile
+
+def register_routes(app):
+    api = Api(app)
+    api.add_resource(FileUpload, "/upload")
+    api.add_resource(AskQuestion, "/ask")
+    api.add_resource(RemoveFile, "/remove")
